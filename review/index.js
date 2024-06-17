796f6e9c4993c0f7cfdff45129f3d1afc10ce7c2
Dromen2001 <dromen2001@gmail.com>
2024-06-17 10:25:43 -0700

    hald way through reviews

---
+++ review/index.js
@@ -0,0 +1,75 @@
+// Use `.map()` to iterate over the following array:
+
+// const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+
+// // Create a new array where each value is multiplied by 2 and log the new array.
+
+// // Your code here
+// const newNums = nums.map((num) => {
+//   return num * 2;
+// })
+// console.log(newNums)
+
+
+// Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
+
+// const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// // Your code here
+// const [firstTopping, secondTopping] = pizzaToppings
+
+// console.log(firstTopping)
+// console.log(secondTopping)
+
+// Given the following object, use destructuring to create variables `make` and `model` that will hold the respective values.
+
+// const car = {
+//   make: 'Audi',
+//   model: 'q5',
+// };
+
+// // Your code here
+// const {make, model} = car
+
+// console.log(car.make);
+// console.log(car.model);
+
+
+// Duplicate the following array using the spread operator and assign it to `controversialPizzaToppings`. Then, log the variable.
+
+// const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// // Your code here
+// const controversialPizzaToppings = [...pizzaToppings]
+// console.log(controversialPizzaToppings)
+
+
+// Duplicate the following object and spread its values into a new variable `myCar`.
+
+// const car = {
+//   make: 'Audi',
+//   model: 'q5',
+// };
+
+// // Change the `model` property of `myCar` to 'q7'. Log both objects.
+
+// // Your code here
+// const myCar = { ...car }
+// console.log(myCar.make);
+// console.log(myCar.model)
+
+
+// Create an object named userProfile. 
+// Define a variable named propertyName and assign a string to it (like a username, age, or email). 
+// Use propertyName as a dynamic key in userProfile, assigning a relevant value.
+
+// Your code here
+
+// const propertyName = 'username'
+// const userProfile = {
+//   [propertyName]: 'Mikey'
+// }
+
+// console.log(userProfile)
+
+
